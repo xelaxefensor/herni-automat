ed96292ef880426b2fd1e0256a89dfd5dadd4653
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2024-12-04 17:59:50 +0100

    interpoaltion

--- games/flappy-mickey/pipes/pipe.gd
+++ games/flappy-mickey/pipes/pipe.gd
@@ -2,22 +2,6 @@ extends RigidBody2D
 
 var move_amount : float = 20
 
-static var id_set = 0
-var id = 0
-
-
-func _ready() -> void:
-	id = id_set
-	id_set+=1
-	
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	position.x -= move_amount * state.step
-	
-
-func _process(delta: float) -> void:
-	if id == 0:
-			print("pipe: "+ str(global_position.x) + "              sprite: "+ str($"../Sprite2D".global_position.x))
-			$"../Sprite2D".global_position.x = lerp($"../Sprite2D".global_position.x, global_position.x, 0.1)
-
-	
